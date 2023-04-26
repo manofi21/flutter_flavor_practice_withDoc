1c20215eace70dcb8ff5fe55a299ed74409a4fd4
unknown <jeremimanogi@gmail.com>
2023-04-26 17:42:28 +0700

    create env.dart for development level

---
+++ lib/env.dart
@@ -0,0 +1,16 @@
+const String envProd = "env_prod";
+const String envUat = "env_uat";
+const String envTraining = "env_training";
+const String envDemo = "env_demo";
+
+/// Same as [envProd], but without `env_`.
+const String prodEnv = "prod";
+
+/// Same as [envUat], but without `env_`.
+const String uatEnv = "uat";
+
+/// Same as [envTraining], but without `env_`.
+const String trainingEnv = "training";
+
+/// Same as [envDemo], but without `env_`.
+const String demoEnv = "demo";
